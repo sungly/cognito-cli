156d0c80cfa05bd2734281be08631f2720044181
Ly Sung <sungly.77@gmail.com>
2020-05-23 12:14:19 -0400

    fix(cli-init): fixed cli init for required attributes

--- src/config.js
+++ src/config.js
@@ -3,14 +3,17 @@ const fs = require('fs');
 const os = require('os');
 
 const homeDir = os.homedir();
-const envConfig = dotenv.parse(fs.readFileSync(`${homeDir}/.cognito/config`));
+const configPath = `${homeDir}/.cognito/config`;
+const envConfig = fs.existsSync(configPath)
+    ? dotenv.parse(fs.readFileSync(configPath))
+    : {};
 
 const _config = {
     userPoolId: envConfig.user_pool_id,
     clientId: envConfig.client_id,
     clientSecret: envConfig.client_secret,
     region: envConfig.region,
-    requiredAttributeList: JSON.parse(envConfig.requiredAttributeList),
+    requiredAttributeList: JSON.parse(envConfig.requiredAttributeList || `{}`),
 };
 
 export default _config;
